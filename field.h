7fe8e34227708c5fecab46c5e65738b6cb8f0017
Alessandro <MY_NAME@kasjdflkjsdflkjsljsfalksjaldskajfghjdfghreunfexample.com>
2023-01-15 18:24:23 +0100

    clang-format

--- field.h
+++ field.h
@@ -5,141 +5,142 @@
 
 class Field {
 public:
-    Field(std::string const &name) : name(name) {}
+  Field(std::string const &name) : name(name) {}
 
-    virtual ~Field() = default;
+  virtual ~Field() = default;
 
-    std::string const getName() {
-        return name;
-    }
+  std::string const getName() {
+    return name;
+  }
 
-    virtual void actionOnPassing(Player &player) = 0;
-    virtual void actionOnStop(Player &player) = 0;
+  virtual void actionOnPassing(Player &player) = 0;
+  virtual void actionOnStop(Player &player) = 0;
 
 private:
-    std::string name;
+  std::string name;
 };
 
 class MatchField : public Field {
 public:
+  MatchField(std::string const &name, int fee, float weight)
+      : Field(name), money(0), fee(fee), weight(weight) {}
 
-    MatchField(std::string const &name, int fee, float weight) : Field(name), money(0), fee(fee), weight(weight) {}
-
-    void actionOnPassing(Player &player) override {
-        if(player.getMoney() < fee) {
-            money += player.getMoney();
-            player.subMoney(player.getMoney());
-            player.game_over();
-        } else {
-            money += fee;
-            player.subMoney(fee);
-        }
+  void actionOnPassing(Player &player) override {
+    if (player.getMoney() < fee) {
+      money += player.getMoney();
+      player.subMoney(player.getMoney());
+      player.game_over();
+    } else {
+      money += fee;
+      player.subMoney(fee);
     }
+  }
 
-    void actionOnStop(Player &player) override {
-        player.addMoney(money * weight);
-        money = 0;
-    }
+  void actionOnStop(Player &player) override {
+    player.addMoney(money * weight);
+    money = 0;
+  }
 
 private:
-    int money;
-    int fee;
-    float weight;
+  int money;
+  int fee;
+  float weight;
 };
 
 class BookmakerField : public Field {
 public:
+  BookmakerField(std::string const &name, int bonus)
+      : Field(name), series(0), bonus(bonus) {}
 
-    BookmakerField(std::string const &name, int bonus) : Field(name), series(0), bonus(bonus) {}
+  void actionOnPassing(Player &player) override {}
 
-    void actionOnPassing(Player &player) override {}
-
-    void actionOnStop(Player &player) override {
-        if (series == 0) {
-            player.addMoney(bonus);
-        } else {
-            if(player.getMoney() < bonus) {
-                player.subMoney(player.getMoney());
-                player.game_over();
-            } else {
-                player.subMoney(bonus);
-            }
-        }
-        series = (series + 1) % 3;
+  void actionOnStop(Player &player) override {
+    if (series == 0) {
+      player.addMoney(bonus);
+    } else {
+      if (player.getMoney() < bonus) {
+        player.subMoney(player.getMoney());
+        player.game_over();
+      } else {
+        player.subMoney(bonus);
+      }
     }
+    series = (series + 1) % 3;
+  }
 
 private:
-    int series;
-    int bonus;
+  int series;
+  int bonus;
 };
 
 class PenaltyField : public Field {
 public:
-    PenaltyField(std::string const &name, int fee) : Field(name), fee(fee) {}
+  PenaltyField(std::string const &name, int fee) : Field(name), fee(fee) {}
 
-    void actionOnPassing(Player &player) override {}
+  void actionOnPassing(Player &player) override {}
 
-    void actionOnStop(Player &player) override {
-        if(player.getMoney() < fee) {
-            player.subMoney(player.getMoney());
-            player.game_over();
-        } else {
-            player.subMoney(fee);
-        }
+  void actionOnStop(Player &player) override {
+    if (player.getMoney() < fee) {
+      player.subMoney(player.getMoney());
+      player.game_over();
+    } else {
+      player.subMoney(fee);
     }
+  }
+
 private:
-    int fee;
+  int fee;
 };
 
 class YellowCardField : public Field {
 public:
-    YellowCardField(std::string const &name, int waiting_time) : Field(name), waiting_time(waiting_time) {}
-    
-    void actionOnPassing(Player &player) override {}
+  YellowCardField(std::string const &name, int waiting_time)
+      : Field(name), waiting_time(waiting_time) {}
 
-    void actionOnStop(Player &player) override {
-        player.setWaitingTime(waiting_time);
-    }
+  void actionOnPassing(Player &player) override {}
+
+  void actionOnStop(Player &player) override {
+    player.setWaitingTime(waiting_time);
+  }
 
 private:
-    int waiting_time;
+  int waiting_time;
 };
-
 
 class SeasonField : public Field {
 public:
-    SeasonField(std::string const &name) : Field(name) {}
+  SeasonField(std::string const &name) : Field(name) {}
 
-    void actionOnPassing(Player &player) override {
-        player.addMoney(50);
-    }
+  void actionOnPassing(Player &player) override { 
+    player.addMoney(50); 
+  }
 
-    void actionOnStop(Player &player) override {
-        player.addMoney(50);
-    }
+  void actionOnStop(Player &player) override { 
+    player.addMoney(50); 
+  }
 };
 
 class GoalField : public Field {
 public:
-    GoalField(std::string const &name, int bonus) : Field(name), bonus(bonus) {}
+  GoalField(std::string const &name, int bonus) : Field(name), bonus(bonus) {}
 
-    void actionOnStop(Player &player) override {
-        player.addMoney(bonus);
-    }
+  void actionOnStop(Player &player) override {  
+    player.addMoney(bonus); 
+  }
 
-    void actionOnPassing(Player &player) override {}
+  void actionOnPassing(Player &player) override {}
 
 private:
-    int bonus;
+  int bonus;
 };
 
 class EmptyField : public Field {
 public:
-    EmptyField(std::string const &name) : Field(name) {}
+  EmptyField(std::string const &name) : Field(name) {}
 
-    void actionOnStop(Player &player) override {}
+  void actionOnStop(Player &player) override {}
 
-    void actionOnPassing(Player &player) override {}
+  void actionOnPassing(Player &player) override {}
 };
 
 #endif
--- worldcup2022.h
+++ worldcup2022.h
@@ -1,118 +1,121 @@
 #ifndef WORLDCUP2022_H
 #define WORLDCUP2022_H
 
-#include "worldcup.h"
-#include "player.h"
-#include "field.h"
-#include "exceptions.h"
 #include "dice.h"
+#include "exceptions.h"
+#include "field.h"
 #include "gameboard.h"
+#include "player.h"
+#include "worldcup.h"
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
-#include <map>
 
 class WorldCup2022 : public WorldCup {
 public:
-    const size_t REQUIRED_DICE = 2;
-    const int START_MONEY = 1000;
+  const size_t REQUIRED_DICE = 2;
+  const int START_MONEY = 1000;
 
-    ~WorldCup2022() = default;
+  ~WorldCup2022() = default;
 
-    void addDie(std::shared_ptr<Die> die) override {
-        dice.addDice(die);
+  void addDie(std::shared_ptr<Die> die) override { 
+    dice.addDice(die); 
+  }
+
+  void addPlayer(std::string const &name) override {
+    Player new_player = Player(name, START_MONEY);
+    players.push_back(new_player);
+    board.addPlayer(new_player);
+  }
+
+  void setScoreBoard(std::shared_ptr<ScoreBoard> scoreboard) override {
+    this->scoreboard = scoreboard;
+  }
+
+  void play(unsigned int rounds) override {
+    if (dice.size() < REQUIRED_DICE) {
+      throw TooFewDiceException();
+    }
+    if (dice.size() > REQUIRED_DICE) {
+      throw TooManyDiceException();
+    }
+    if (players.size() > 11) {
+      throw TooManyPlayersException();
+    }
+    if (players.size() < 2) {
+      throw TooFewPlayersException();
     }
 
-    void addPlayer(std::string const &name) override {
-        Player new_player = Player(name,START_MONEY);
-        players.push_back(new_player);
-        board.addPlayer(new_player);
+    initBoard();
+    std::pair<int, std::string> money_winner;
+    for (unsigned int round_num = 0; round_num < rounds; round_num++) {
+      scoreboard->onRound(round_num);
+      for (auto &player : players) {
+        if (!player.isInGame()) {
+          continue;
+        }
+        if (!player.isWaiting()) {
+          int sum = dice.roll();
+          board.move(player, sum);
+        }
+        scoreboard->onTurn(player.getName(), player.getStatus(),
+                           board.getPlayersPosition(player), player.getMoney());
+        if (player.isWaiting()) {
+          player.decreaseWaitingTime();
+        }
+
+        int alive_num = 0;
+        money_winner = {0, ""};
+        for (auto &pl : players) {
+          money_winner = std::max(money_winner, {pl.getMoney(), pl.getName()});
+          if (pl.isInGame()) {
+            alive_num++;
+          }
+        }
+        if (alive_num == 1) {
+          scoreboard->onWin(money_winner.second);
+          return;
+        }
+      }
     }
-
-    void setScoreBoard(std::shared_ptr<ScoreBoard> scoreboard) override {
-        this->scoreboard = scoreboard;
-    }
-
-    void play(unsigned int rounds) override {
-        if (dice.size() < REQUIRED_DICE) {
-            throw TooFewDiceException();
-        }
-        if (dice.size() > REQUIRED_DICE) {
-            throw TooManyDiceException();
-        }
-        if (players.size() > 11) {
-            throw TooManyPlayersException();
-        }
-        if (players.size() < 2) {
-            throw TooFewPlayersException();
-        }
-
-        initBoard();
-        std::pair<int,std::string> money_winner;
-        for (unsigned int round_num = 0; round_num < rounds; round_num++) {
-            scoreboard->onRound(round_num);
-            for (auto &player : players) {
-                if (!player.isInGame()) {
-                    continue;
-                }
-                if (!player.isWaiting()) {
-                    int sum = dice.roll();
-                    board.move(player, sum);
-                }
-                scoreboard->onTurn(player.getName(), player.getStatus(), board.getPlayersPosition(player), player.getMoney());
-                if (player.isWaiting()) {
-                    player.decreaseWaitingTime();
-                }
-
-                int alive_num = 0;
-                money_winner = {0,""};
-                for (auto &pl : players) {
-                    money_winner = std::max(money_winner, {pl.getMoney(), pl.getName()});
-                    if (pl.isInGame()) {
-                        alive_num++;
-                    }
-                }
-                if (alive_num == 1) {
-                    scoreboard->onWin(money_winner.second);
-                    return;
-                }
-            }
-        }   
-        scoreboard->onWin(money_winner.second);
-    }
+    scoreboard->onWin(money_winner.second);
+  }
 
 private:
-    std::shared_ptr<ScoreBoard> scoreboard;
-    Dice dice;
-    std::vector<Player> players;
-    Gameboard board;
+  std::shared_ptr<ScoreBoard> scoreboard;
+  Dice dice;
+  std::vector<Player> players;
+  Gameboard board;
 
-    void initBoard() {
-        board.addField(std::make_shared<SeasonField>("Początek sezonu"));
-        
-        board.addField(std::make_shared<MatchField>("Mecz z San Marino",160,1));
-        
-        board.addField(std::make_shared<EmptyField>("Dzień wolny od treningu"));
-    
-        board.addField(std::make_shared<MatchField>("Mecz z Lichtensteinem",220,1));
+  void initBoard() {
+    board.addField(std::make_shared<SeasonField>("Początek sezonu"));
 
-        board.addField(std::make_shared<YellowCardField>("Żółta kartka",3));
+    board.addField(std::make_shared<MatchField>("Mecz z San Marino", 160, 1));
 
-        board.addField(std::make_shared<MatchField>("Mecz z Meksykiem",300,2.5));
+    board.addField(std::make_shared<EmptyField>("Dzień wolny od treningu"));
 
-        board.addField(std::make_shared<MatchField>("Mecz z Arabią Saudyjską",280,2.5));
+    board.addField(
+        std::make_shared<MatchField>("Mecz z Lichtensteinem", 220, 1));
 
-        board.addField(std::make_shared<BookmakerField>("Bukmacher",100));
+    board.addField(std::make_shared<YellowCardField>("Żółta kartka", 3));
 
-        board.addField(std::make_shared<MatchField>("Mecz z Argentyną",250,2.5));
+    board.addField(std::make_shared<MatchField>("Mecz z Meksykiem", 300, 2.5));
 
-        board.addField(std::make_shared<GoalField>("Gol",120));
-        
-        board.addField(std::make_shared<MatchField>("Mecz z Francją",400,4));
-        
-        board.addField(std::make_shared<PenaltyField>("Rzut karny",180));
-    }
+    board.addField(
+        std::make_shared<MatchField>("Mecz z Arabią Saudyjską", 280, 2.5));
+
+    board.addField(std::make_shared<BookmakerField>("Bukmacher", 100));
+
+    board.addField(std::make_shared<MatchField>("Mecz z Argentyną", 250, 2.5));
+
+    board.addField(std::make_shared<GoalField>("Gol", 120));
+
+    board.addField(std::make_shared<MatchField>("Mecz z Francją", 400, 4));
+
+    board.addField(std::make_shared<PenaltyField>("Rzut karny", 180));
+  }
 };
 
 #endif
